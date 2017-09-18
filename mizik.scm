80837cde08a5339d2201a0ffa5e9ec6e99877ebe
WammKD <jaft.r@outlook.com>
2017-09-17 22:49:50 -0500

    Made a top-bar window. I'm going to have to consider how I want to handle managing it. I'm not entirely certain what I want this player to look like, come to think of it. Updating columns for the top-bar's going to be a pain. I need a programmatic way of handling it.

--- mizik.scm
+++ mizik.scm
@@ -3,10 +3,26 @@
 (use-modules (mpd)
              (ncurses curses))
 
-(define stdscr (initscr))
+(setlocale LC_ALL "")
 
-(let main ()
-  (addstr  stdscr "Hello!")
-  (refresh stdscr))
+(define stdscr (initscr))
+(noecho!)
+(cbreak!)
+(start-color!)
+(keypad! stdscr #t)
+(refresh stdscr)
+
+(define header (newwin 1 (cadr (getmaxyx stdscr)) 0 0))
+(chgat header -1 A_REVERSE 0)
+(addstr header "Blink Don't Blink")
+(refresh header)
+
+(let main [(past_dimensions (getmaxyx stdscr))]
+  (move stdscr 1 0)
+  (refresh stdscr)
+  (getch   stdscr)
+  (getch   stdscr)
+  (getch   stdscr)
+  (getch   stdscr))
 
 (endwin)
