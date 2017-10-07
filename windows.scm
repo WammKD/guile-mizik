09aa6a727a0a5c7785f5431fd29f23045485d5f6
WammKD <jaft.r@outlook.com>
2017-10-07 16:58:10 -0500

    Even though the windows will sort of unnecessarily be tossed from lambda to lambda, I can better store values in a functional way by generating a new lambda to return for each call. I just realized that I didn't actually save every call to #:rebuild in the main loop and it still functions, great; a consequence of the size and offsets not being dependent on the old values and the imperetive nature of the main containing_window such that it records its own changing width and height.

--- windows.scm
+++ windows.scm
@@ -3,36 +3,40 @@
 (use-modules (ncurses curses)
              (srfi srfi-1))
 
-(define* (windows::build-header-bar containing_window caption)
+(define* (windows::build-header-bar containing_window new_window caption)
   (define (transform-caption)
     (let [(len      (getmaxx containing_window))
           (ELLIPSIS                         "…")]
-      (if (< len 2)
+      (if (= len 0)
           ""
-        (if (= len 2)
+        (if (or (= len 1) (= len 2))
             ELLIPSIS
           (if (> (string-length caption) (- len 1))
               (string-append (substring caption 0 (- len 2)) ELLIPSIS)
             caption)))))
 
-  (define window (let* [(len    (getmaxx containing_window))
-                        (header (newwin
-                                  1
-                                  len
-                                  (getbegy containing_window)
-                                  (getbegx containing_window)))]
-                   (addstr header (transform-caption))
-                   (chgat header -1 A_REVERSE 0 #:x 0 #:y 0)
+  (define window (if new_window
+                     new_window
+                   (let* [(len    (getmaxx containing_window))
+                          (header (newwin
+                                    1
+                                    len
+                                    (getbegy containing_window)
+                                    (getbegx containing_window)))]
+                     (addstr header (transform-caption))
+                     (chgat header -1 A_REVERSE 0 #:x 0 #:y 0)
                    
-                   (refresh header)
+                     (refresh header)
                    
-                   header))
+                     header)))
 
   (lambda (method . xs)
     (cond
-     [(eq? method #:get-caption) caption]
-     [(eq? method #:get-window)  window]
-     [(eq? method #:get-maxyx)   (getmaxyx window)]
+     [(eq? method #:get-caption)           caption]
+     [(eq? method #:get-window)             window]
+     [(eq? method #:get-max-y-x) (getmaxyx window)]
+     [(eq? method #:get-max-y)   (getmaxy  window)]
+     [(eq? method #:get-max-x)   (getmaxx  window)]
      [(eq? method #:rebuild)     (resize
                                    window
                                    1
@@ -41,10 +45,18 @@
                                    window
                                    (getbegy containing_window)
                                    (getbegx containing_window))
+
                                  (erase window)
+
                                  (addstr window (transform-caption))
                                  (chgat window -1 A_REVERSE 0 #:x 0 #:y 0)
-                                 (refresh window)]
+
+                                 (refresh window)
+
+                                 (windows::build-header-bar
+                                   containing_window
+                                   window
+                                   caption)]
      [(eq? method #:erase)       (let [(pos (getyx containing_window))]
                                    (erase   window)
                                    (refresh window)
@@ -95,22 +107,25 @@
                               (temp
                                 (cons
                                   (container
+                                    containing_window
                                     win
-                                    (windows::build-header-bar win cc)
+                                    (windows::build-header-bar win #f cc)
                                     ratio)
                                   result)
                                 (cdr current)
                                 (+ offset rounded)))))))))
 
-  (define (container win header_bar percentage)
+  (define (container containing_win win header_bar percentage)
     (lambda (method . xs)
       (cond
        [(eq? method #:get-header-object)     header_bar]
        [(eq? method #:get-window)                   win]
-       [(eq? method #:get-maxyx)         (getmaxyx win)]
+       [(eq? method #:get-max-y-x)       (getmaxyx win)]
+       [(eq? method #:get-max-y)         (getmaxy  win)]
+       [(eq? method #:get-max-x)         (getmaxx  win)]
        [(eq? method #:get-ratio)             percentage]
        [(eq? method #:rebuild)           (let* [(cw_len   (getmaxx
-                                                            containing_window))
+                                                            containing_win))
                                                 (offset   (car  xs))
                                                 (last_w?  (cadr xs))
                                                 (nw_width (inexact->exact
@@ -120,20 +135,22 @@
                                                                 cw_len))))]
                                            (resize
                                              win
-                                             (getmaxy containing_window)
+                                             (getmaxy containing_win)
                                              (if last_w?
                                                  (- cw_len offset)
                                                nw_width))
                                            (mvwin
                                              win
-                                             (getbegy containing_window)
+                                             (getbegy containing_win)
                                              offset)
                                            (refresh win)
 
-                                           (header_bar #:rebuild)
-
-                                           (+ nw_width offset))]
-       [(eq? method #:erase)             (let [(pos (getyx containing_window))]
+                                           (container
+                                             containing_win
+                                             win
+                                             (header_bar #:rebuild)
+                                             percentage))]
+       [(eq? method #:erase)             (let [(pos (getyx containing_win))]
                                            (header_bar #:erase)
 
                                            (erase   win)
@@ -142,23 +159,30 @@
                                            (delwin  win)
 
                                            (move
-                                             containing_window
+                                             containing_win
                                              (car  pos)
                                              (cadr pos)))])))
 
   (lambda (method . xs)
     (cond
      [(eq? method #:get-windows) windows]
-     [(eq? method #:rebuild)     (let temp [(offset          0)
-                                            (remaining windows)]
-                                   (if (null? (cdr remaining))
-                                       ((car remaining) #:rebuild
-                                                          offset
-                                                          #t)
-                                     (temp
-                                       ((car remaining) #:rebuild
-                                                          offset
-                                                          #f)
-                                       (cdr remaining))))]
+     [(eq? method #:rebuild)     (windows::build-main-window
+                                   containing_window
+                                   (let temp [(offset          0)
+                                              (remaining windows)
+                                              (final         '())]
+                                     (if (null? (cdr remaining))
+                                         (reverse (cons
+                                                    ((car remaining) #:rebuild
+                                                                       offset
+                                                                       #t)
+                                                    final))
+                                       (let [(win ((car remaining) #:rebuild
+                                                                     offset
+                                                                     #f))]
+                                         (temp
+                                           (+ offset (win #:get-max-x))
+                                           (cdr remaining)
+                                           (cons win final))))))]
      [(eq? method #:erase)       (for-each (lambda (x)
                                              (x #:erase)) windows)])))
