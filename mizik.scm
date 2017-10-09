c1ed50c8411a1f0c7c7b01e2047ab928e9b26ae1
WammKD <jaft.r@outlook.com>
2017-10-08 20:41:20 -0500

    Just a bit messy but should handle fully adding elements to the columns. Basically, hand it an a-list and it'll place the cdr in the column whose header matches the car. Each line is stored in each column window which allows for them to be kept track of. I'll have to add handling when the list of lines is longer than the screen height (that's part of what highlight_pos is there for) and what it should do if that line is selected (I'm thinking of the colums-managing-lambda keeping its own list of lambda-commands associated with each line so it know what to do if a line is selected). Then – of course –, I'll need to handle a line-highlighting and moving said highlighting; I'll likely do that next.

--- mizik.scm
+++ mizik.scm
@@ -32,9 +32,34 @@
                         (main_window #:rebuild)
                       main_window))
            (char    (getch (new_win #:get-con-window)))]
-      
-
-      (when (not (equal? char #\q))
-        (main new_win new_past_dimensions)))))
+      (cond
+       [(equal? char #\a) (main (new_win
+                                  #:add-line
+                                    '((file . "Born to Run/Bruce Springsteen (Born to Run) - 01 - Thunder Road.mp3")
+                                      (Last-Modified . "2017-06-04T19:17:11Z")
+                                      (Time . "291")
+                                      (Artist . "Bruce Springsteen")
+                                      (AlbumArtist . "Bruce Springsteen")
+                                      (Title . "Thunder Road")
+                                      (Album . "Born to Run")
+                                      (Track . "1/8")
+                                      (Date . "1975")
+                                      (Genre . "Rock")
+                                      (Composer . "Bruce Springsteen")
+                                      (Disc . "1")
+                                      (MUSICBRAINZ_ARTISTID . "70248960-cb53-4ea4-943a-edb18f7d336f"))) new_past_dimensions)]
+       [(not (equal? char #\q)) (main new_win new_past_dimensions)]))))
 
 (endwin)
+
+
+(mpd-connect client)
+
+(display (get-mpd-response (mpdDatabase::ls-info client "Born to Run")))
+;; (display (get-mpd-response
+;;            (mpdDatabase::ls-info
+;;              client
+;;              (cdadar (get-mpd-response (mpdDatabase::list-all client))))))
+(newline)
+
+(mpd-disconnect client)
--- windows.scm
+++ windows.scm
@@ -4,16 +4,16 @@
              (srfi srfi-1))
 
 (define* (windows::build-header-bar containing_window new_window caption)
-  (define (transform-caption)
+  (define (transform-line line)
     (let [(len      (getmaxx containing_window))
           (ELLIPSIS                         "…")]
       (if (= len 0)
           ""
         (if (or (= len 1) (= len 2))
             ELLIPSIS
-          (if (> (string-length caption) (- len 1))
-              (string-append (substring caption 0 (- len 2)) ELLIPSIS)
-            caption)))))
+          (if (> (string-length line) (- len 1))
+              (string-append (substring line 0 (- len 2)) ELLIPSIS)
+            line)))))
 
   (define window (if new_window
                      new_window
@@ -23,7 +23,7 @@
                                     len
                                     (getbegy containing_window)
                                     (getbegx containing_window)))]
-                     (addstr header (transform-caption))
+                     (addstr header (transform-line caption))
                      (chgat header -1 A_REVERSE 0 #:x 0 #:y 0)
                    
                      (refresh header)
@@ -32,11 +32,12 @@
 
   (lambda (method . xs)
     (cond
-     [(eq? method #:get-caption)           caption]
-     [(eq? method #:get-window)             window]
-     [(eq? method #:get-max-y-x) (getmaxyx window)]
-     [(eq? method #:get-max-y)   (getmaxy  window)]
-     [(eq? method #:get-max-x)   (getmaxx  window)]
+     [(eq? method #:get-caption)                   caption]
+     [(eq? method #:get-window)                     window]
+     [(eq? method #:get-max-y-x)         (getmaxyx window)]
+     [(eq? method #:get-max-y)           (getmaxy  window)]
+     [(eq? method #:get-max-x)           (getmaxx  window)]
+     [(eq? method #:transform)   (transform-line (car xs))]
      [(eq? method #:rebuild)     (resize
                                    window
                                    1
@@ -48,7 +49,7 @@
 
                                  (erase window)
 
-                                 (addstr window (transform-caption))
+                                 (addstr window (transform-line caption))
                                  (chgat window -1 A_REVERSE 0 #:x 0 #:y 0)
 
                                  (refresh window)
@@ -110,20 +111,38 @@
                                     containing_window
                                     win
                                     (windows::build-header-bar win #f cc)
+                                    '()
+                                    -1
                                     ratio)
                                   result)
                                 (cdr current)
                                 (+ offset rounded)))))))))
 
-  (define (container containing_win win header_bar percentage)
+  (define (container containing_win win           header_bar
+                     lines          highlight_pos percentage)
     (lambda (method . xs)
       (cond
-       [(eq? method #:get-header-object)     header_bar]
-       [(eq? method #:get-window)                   win]
-       [(eq? method #:get-max-y-x)       (getmaxyx win)]
-       [(eq? method #:get-max-y)         (getmaxy  win)]
-       [(eq? method #:get-max-x)         (getmaxx  win)]
-       [(eq? method #:get-ratio)             percentage]
+       [(eq? method #:get-header-object)                 header_bar]
+       [(eq? method #:get-caption)       (header_bar #:get-caption)]
+       [(eq? method #:get-window)                               win]
+       [(eq? method #:get-max-y-x)                   (getmaxyx win)]
+       [(eq? method #:get-max-y)                     (getmaxy  win)]
+       [(eq? method #:get-max-x)                     (getmaxx  win)]
+       [(eq? method #:get-ratio)                         percentage]
+       [(eq? method #:add-line)          (addstr
+                                           win
+                                           (header_bar #:transform (car xs))
+                                           #:y (+ 1 (length lines))
+                                           #:x 0)
+                                         (refresh win)
+
+                                         (container
+                                           containing_win
+                                           win
+                                           (header_bar #:rebuild)
+                                           (append lines (list (car xs)))
+                                           highlight_pos
+                                           percentage)]
        [(eq? method #:rebuild)           (let* [(cw_len   (getmaxx
                                                             containing_win))
                                                 (offset   (car  xs))
@@ -143,12 +162,28 @@
                                              win
                                              (getbegy containing_win)
                                              offset)
+                                           (erase win)
+
+                                           (let loop [(elems lines)
+                                                      (index     1)]
+                                             (when (not (null? elems))
+                                               (addstr
+                                                 win
+                                                 (header_bar #:transform
+                                                               (car elems))
+                                                 #:y index
+                                                 #:x 0)
+
+                                               (loop (cdr elems) (1+ index))))
+
                                            (refresh win)
 
                                            (container
                                              containing_win
                                              win
                                              (header_bar #:rebuild)
+                                             lines
+                                             highlight_pos
                                              percentage))]
        [(eq? method #:erase)             (let [(pos (getyx containing_win))]
                                            (header_bar #:erase)
@@ -171,6 +206,23 @@
      [(eq? method #:get-con-max-y)   (getmaxy  containing_window)]
      [(eq? method #:get-con-max-x)   (getmaxx  containing_window)]
      [(eq? method #:refresh-contain) (refresh  containing_window)]
+     [(eq? method #:add-line)        (windows::build-main-window
+                                       containing_window
+                                       (map
+                                         (lambda (window)
+                                           (window
+                                             #:add-line
+                                             (let loop [(elements (car xs))]
+                                               (if (null? elements)
+                                                   ""
+                                                 (if (eq?
+                                                       (string->symbol
+                                                         (window
+                                                           #:get-caption))
+                                                       (caar elements))
+                                                     (cdar elements)
+                                                   (loop (cdr elements)))))))
+                                         windows))]
      [(eq? method #:rebuild)         (windows::build-main-window
                                        containing_window
                                        (let temp [(offset          0)
